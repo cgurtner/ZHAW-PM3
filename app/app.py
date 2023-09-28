1303c040340f040280a96059f42d51f1e6c114e3
Cyrill Gurtner <me@cgurtner.ch>
2023-09-28 20:37:44 +0200

    dummy static user.html

--- app/app.py
+++ app/app.py
@@ -6,5 +6,36 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/search/<id>')
+def search(id):
+    return 'You are searching: ' + id
+
+@app.route('/api/users')
+def users():
+    return {
+            "users": [
+            {
+                "name": "John",
+                "surname": "Doe",
+                "age": 30
+            },
+            {
+                "name": "Jane",
+                "surname": "Smith",
+                "age": 25
+            },
+            {
+                "name": "Mike",
+                "surname": "Jordan",
+                "age": 55
+            },
+            {
+                "name": "Alice",
+                "surname": "Johnson",
+                "age": 35
+            }
+        ]
+    }
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
