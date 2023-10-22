df2ea362465b94a26d5f41438b461c5895505994
Cyrill Gurtner <me@cgurtner.ch>
2023-10-22 21:32:40 +0200

    store API URLs in .env.local

--- frontend/app/explore/page.js
+++ frontend/app/explore/page.js
@@ -1,7 +1,7 @@
 import AmenityList from "./components/AmenityList";
 
 export default async function Explore() {
-  const res = await fetch('http://api:5000/api/amenity-types')
+  const res = await fetch(process.env.NEXT_PUBLIC_API_SERVER_URL + 'amenity-types')
   const data = await res.json()
 
   const sortedList = Object.entries(data).sort((a, b) => b[1] - a[1]);
