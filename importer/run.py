6943f3c64f39fb93df037e7067dc8fc37eb45077
Cyrill Gurtner <me@cgurtner.ch>
2023-10-22 16:09:20 +0200

    refs #3 | added some data cleaning functions

--- importer/run.py
+++ importer/run.py
@@ -1,19 +1,58 @@
-import json
+import json, os, copy
 from pymongo import MongoClient
+from helper import cast, delField, compare
 
 # MongoDB connection string
 mongo_uri = "mongodb://mongodb:27017/"
 client = MongoClient(mongo_uri)
 
+filepath = os.path.join(os.getcwd(), 'data/osm-output.json')
+
+with open(filepath, 'r') as file:
+    osm = json.load(file)
+
+cleaned_data, removed_rows_count = [], 0
+for entry in osm['nodes']:
+    # we don't want to alter the original entry in osm['nodes'] for further comparions and tests
+    entry = copy.deepcopy(entry)
+    # change all keys to lowercase
+    entry = {k.lower(): v for k, v in entry.items()}
+
+    # rows with fixme attribute should be removed
+    # fixme is used to mark rows with errors or wrong data
+    # https://wiki.openstreetmap.org/wiki/Key:fixme
+    # test row: 257085086, 267053813, 271846258
+    if 'fixme' in entry:
+        removed_rows_count += 1
+        continue
+
+    # rows have more than one attribute to indicate a website
+    # "website":
+    # "contact:website":
+    # "url":
+    # according to the wiki, the priority is "website":, "contact:website":, "url":
+    # "url": is the most open tag: https://wiki.openstreetmap.org/wiki/Key:url
+    # therefore we save in that order, which is available in "website":
+    # test 1: 1014954, has url and website => url should be removed, website should stay the same
+    # test 2: 3020881761, has website and contact:website => website should be taken, contact:website removed
+    # test 3: 298715804, has only url => should be changed to website:
+    website = False
+    if 'website' in entry:
+        website = entry['website']
+    elif 'contact:website' in entry:
+        website = entry['contact:website']
+    elif 'url' in entry:
+        website = entry['url']
+
+    entry = delField('contact:website', entry)
+    entry = delField('url', entry)
+
+    entry['website'] = website
+
+    # datatype casts
+    entry = cast('id', 'int', entry)
+    entry = cast('lat', 'float', entry)
+    entry = cast('lon', 'float', entry)
+    cleaned_data.append(entry)
+
 db = client.get_database('pm3')
-collection = db.get_collection('users')
-
-user_data = [
-    {"name": "John", "surname": "Doe", "age": 30},
-    {"name": "Jane", "surname": "Smith", "age": 25},
-    {"name": "Mike", "surname": "Jordan", "age": 55},
-    {"name": "Alice", "surname": "Johnson", "age": 35},
-]
-
-collection.insert_many(user_data)
-print(f"Inserted {len(user_data)} users into the collection.")
