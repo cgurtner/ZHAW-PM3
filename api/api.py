cae090f0d799c9f9def4800db3923a2627379861
Cyrill Gurtner <me@cgurtner.ch>
2023-10-22 17:40:07 +0200

    cleanup api.py; fetch amenities by type

--- api/api.py
+++ api/api.py
@@ -1,27 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# mongodb client
 client = MongoClient('mongodb://mongodb:27017/')  
-db = client.pm3
+db = client.osm
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/search/<id>')
-def search(id):
-    return 'You are searching: ' + id
-
-@app.route('/api/users')
-def users():
-    user_collection = db.users
-    users = user_collection.find()
-    
-    users_list = {'users': [{'name': user['name'], 'surname': user['surname'], 'age': user['age']} for user in users]}
-    return users_list
+@app.route('/api/amenities/<type>')
+def amenities(type):
+    amenities = list(db.amenities.find({'amenity': type}, {'_id': 0}))
+    return jsonify(amenities)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
