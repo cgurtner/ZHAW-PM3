028cfbe0608df0456616eccb211a3f9cea8f786b
Cyrill Gurtner <me@cgurtner.ch>
2023-10-22 18:57:41 +0200

    endpoints to explore amenities and their attributes

--- api/api.py
+++ api/api.py
@@ -3,13 +3,39 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://mongodb:27017/')  
+client = MongoClient('mongodb://mongodb:27017/')
 db = client.osm
 
-@app.route('/api/amenities/<type>')
-def amenities(type):
+
+@app.route('/api/amenity-types')
+def amenities():
+    condition = [
+        {
+            "$group": {
+                "_id": "$amenity",
+                "count": {"$sum": 1}
+            }
+        }
+    ]
+
+    res = db.amenities.aggregate(condition)
+    res = {item['_id']: item['count'] for item in res}
+
+    return res
+
+
+@app.route('/api/explore/<type>')
+def explore(type):
     amenities = list(db.amenities.find({'amenity': type}, {'_id': 0}))
-    return jsonify(amenities)
+    resp = {}
+    for amenity in amenities:
+        resp['overall_count'] = resp.get('overall_count', 0) + 1
+        for key in amenity:
+            if key == 'amenity':
+                continue
+            resp[key] = resp.get(key, 0) + 1
+    return resp
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
