d51d6d357392e5c836e54847a8fe051b571016b0
Cyrill Gurtner <me@cgurtner.ch>
2023-10-22 21:09:19 +0200

    enable CORS to support Next.js client- and server components

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient('mongodb://mongodb:27017/')
 db = client.osm
