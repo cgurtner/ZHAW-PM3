5381ca817671522ee88f04a1622ace0df815cc72
Cyrill Gurtner <me@cgurtner.ch>
2023-10-29 12:28:09 +0100

    change nav-logo; refactor import of NavBar

--- frontend/app/NavBar.js
+++ frontend/app/NavBar.js
@@ -11,10 +11,8 @@ const NavBar = ({ amenity, setAmenity }) => {
         <div className="w-full bg-dark-dh text-white p-4 justify-between mb-12 shadow-lg">
             <div className="container flex justify-between mx-auto">
                 <div className="px-3 py-1 flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#00d68a" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
-                    </svg>&nbsp;
-                    YummiTummySearchy
+                    <img src="/nav-logo.png" alt="Logo NightNav" className="inline h-5" />&nbsp;
+                    NightNav
                 </div>
                 <nav>
                     <ul className="flex space-x-6">
--- frontend/app/explore/page.js
+++ frontend/app/explore/page.js
@@ -1,14 +1,32 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import NavBar from "../NavBar";
 import AmenityList from "./AmenityList";
 
-export default async function Explore() {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_SERVER_URL + 'amenity-types')
-  const data = await res.json()
+export default function Explore() {
+  const [amenityTypes, setAmenityTypes] = useState([])
 
-  const sortedList = Object.entries(data).sort((a, b) => b[1] - a[1]);
+  const fetchAmenityTypes = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_CLIENT_URL + 'amenity-types')
+    const data = await res.json()
+
+    setAmenityTypes(Object.entries(data).sort((a, b) => b[1] - a[1]));
+  }
+
+  useEffect(() => {
+    fetchAmenityTypes()
+  }, [])
 
   return (
-    <div className="container">
-      <AmenityList sortedList={sortedList} /> 
-    </div>
+    <main>
+      <NavBar amenity={null} setAmenity={() => { return }} />
+      <div className="flex justify-center">
+        <div className="container">
+          <AmenityList sortedList={amenityTypes} />
+        </div>
+      </div>
+    </main>
+
   )
 }
