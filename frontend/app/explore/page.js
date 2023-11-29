21e11298943b8145e89af5b8426d7c53d14eae86
Cyrill Gurtner <me@cgurtner.ch>
2023-11-29 17:34:43 +0100

    remove explore page content and AmenityList

--- frontend/app/explore/page.js
+++ frontend/app/explore/page.js
@@ -1,29 +1,14 @@
 'use client'
 
-import { useState, useEffect } from "react";
 import NavBar from "../NavBar";
-import AmenityList from "./AmenityList";
 
 export default function Explore() {
-  const [amenityTypes, setAmenityTypes] = useState([])
-
-  const fetchAmenityTypes = async () => {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_CLIENT_URL + 'amenity-types')
-    const data = await res.json()
-
-    setAmenityTypes(Object.entries(data).sort((a, b) => b[1] - a[1]));
-  }
-
-  useEffect(() => {
-    fetchAmenityTypes()
-  }, [])
-
   return (
     <main>
       <NavBar amenity={null} setAmenity={() => { return }} />
       <div className="flex justify-center">
         <div className="container">
-          <AmenityList sortedList={amenityTypes} />
+          Hello Explore Page!
         </div>
       </div>
     </main>
