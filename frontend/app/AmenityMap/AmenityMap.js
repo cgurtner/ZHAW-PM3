5bb8f13b029f5dc1b170cfc14858610b963fab40
Cyrill Gurtner <me@cgurtner.ch>
2023-12-07 23:28:48 +0100

    scale map to always include red and blue marker

--- frontend/app/AmenityMap/AmenityMap.js
+++ frontend/app/AmenityMap/AmenityMap.js
@@ -36,7 +36,23 @@ export default function AmenityMap({amenity, myLocation}) {
             shadowSize: [41, 41]
         });
 
-        L.marker([amenity.lat, amenity.lon], { icon: redIcon }).addTo(map);
+        const blueIcon = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        const myMarker = L.marker([myLocation.latitude, myLocation.longitude], { icon: redIcon }).addTo(map);
+        const amenityMarker = L.marker([amenity.lat, amenity.lon], { icon: blueIcon }).addTo(map);
+        const group = new L.featureGroup([myMarker, amenityMarker]);
+
+        map.fitBounds(group.getBounds(), {
+            padding: [50, 50]
+        });
+
         setMapInstance(map)
 
         return () => {
