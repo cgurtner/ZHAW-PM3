11e2089187dcbf206dd2f6927edd06e26e6bae61
Cyrill Gurtner <me@cgurtner.ch>
2023-12-09 22:05:06 +0100

    refs #6 | show amenity where the user is possible in

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import LocationFetch from './LocationFetch';
 import Amenity from './Amenity/Amenity';
@@ -9,6 +9,7 @@ export default function Home() {
   const [amenity, setAmenity] = useState(false);
   const [selectedCuisine, setSelectedCuisine] = useState('All');
   const [location, setLocation] = useState();
+  const [inLocation, setInLocation] = useState();
 
   const fetchAmenity = async (id) => {
     try {
@@ -20,6 +21,24 @@ export default function Home() {
     }
   }
 
+  const searchInLocation = async () => {
+    const url = `${process.env.NEXT_PUBLIC_API_CLIENT_URL}nearby?lat=${location.latitude}&lon=${location.longitude}&types=${['restaurant', 'cafe', 'fast_food', 'biergarten'].join(',')}&distance=5`;
+    const response = await fetch(url);
+    let data = await response.json();
+    if (data.length > 0) {
+      setInLocation(data[0])
+    }
+  }
+
+  useEffect(() => {
+    if (!location) {
+      return
+    }
+    if (!inLocation) {
+      searchInLocation()
+    }
+  })
+
   return (
     <main>
       <NavBar amenity={amenity} setAmenity={setAmenity} />
@@ -28,6 +47,8 @@ export default function Home() {
           <PageAmenity amenity={amenity} location={location} />
         ) : (
           <PageNearby
+            setAmenity={setAmenity}
+            inLocation={inLocation}
             fetchAmenity={fetchAmenity}
             selectedCuisine={selectedCuisine}
             onCuisineChange={setSelectedCuisine}
@@ -40,7 +61,7 @@ export default function Home() {
   );
 }
 
-const PageNearby = ({ fetchAmenity, selectedCuisine, onCuisineChange, location, setLocation }) => (
+const PageNearby = ({ inLocation, fetchAmenity, selectedCuisine, onCuisineChange, location, setLocation }) => (
   <div className="container mt-12">
     <div className="grid grid-cols-1">
       <div className="flex justify-center text-8xl mb-12 font-semibold">
@@ -49,6 +70,19 @@ const PageNearby = ({ fetchAmenity, selectedCuisine, onCuisineChange, location, 
       <div className="flex justify-center text-4xl mb-12">
         <h2>You give us a click, we give you a dining experience!</h2>
       </div>
+      {
+        inLocation ? (
+          <div className="flex justify-center text-4xl mb-12 items-center">
+            <h2>Are you here?</h2>&nbsp;
+            <button
+                className="text-4xl bg-dark-dh hover:bg-light-dh text-white font-bold py-4 px-8"
+                onClick={() => {fetchAmenity(inLocation.id)}}
+              >
+                {inLocation.name}
+              </button>
+          </div>
+        ) : <></>
+      }
       <div className="flex justify-center mb-12">
         <LocationFetch
           setAmenity={fetchAmenity}
