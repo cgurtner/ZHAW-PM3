8c83b85b94cbd508eab60a307e90fc0b4b82fd65
Cyrill Gurtner <me@cgurtner.ch>
2023-12-13 19:49:46 +0100

    amenity details cols dynamic

--- frontend/app/Amenity/Amenity.js
+++ frontend/app/Amenity/Amenity.js
@@ -35,6 +35,7 @@ export default function Amenity({ amenity, myLocation, allAmenitiesData }) {
   const street = amenity.address['addr:street'] + (amenity.address['addr:housenumber'] ? ' ' + amenity.address['addr:housenumber'] : '')
   const city = amenity.address['addr:postcode'] + ' ' + amenity.address['addr:city']
   const cuisine = amenity.cuisine.length > 0 ? amenity.cuisine.join(', ') : undefined
+  const showContact = website != null || phone != null || email != null ? true : false
 
   const hours = amenity.opening_hours ? amenity.opening_hours.split(';') : []
   const openingHours = amenity.opening_hours ? <>{
@@ -84,12 +85,20 @@ export default function Amenity({ amenity, myLocation, allAmenitiesData }) {
     }
   };
 
+  let numCols = 4;
+  if (hours.length == 0) {
+    numCols--
+  }
+  if (!showContact) {
+    numCols--
+  }
+
   return (
     <>
       <div className="flex justify-center text-center text-6xl md:text-8xl mb-12 font-semibold">
         <h1>{amenity.name}</h1>
       </div>
-      <div className={"bg-dark-dh mb-6 p-3 text-white grid grid-cols-2 gap-y-3 " + (hours.length > 0 ? "md:grid-cols-4" : "md:grid-cols-3 md:gap-48")}>
+      <div className={"bg-dark-dh flex mb-6 p-3 text-white grid grid-cols-2 gap-y-3 " + ("md:grid-cols-" + numCols + (numCols < 4 ? " justify-between": ""))}>
         <div>
           <h2 className="font-semibold text-2xl mb-3">Address</h2>
           <span className="font-semibold">{amenity.name}</span><br />
@@ -103,12 +112,16 @@ export default function Amenity({ amenity, myLocation, allAmenitiesData }) {
           {street}<br />
           {city}
         </div>
-        <div>
-          <h2 className="font-semibold text-2xl mb-3">Contact</h2>
-          {phone}
-          {email}
-          {website}
-        </div>
+        {
+          showContact ? (
+            <div>
+              <h2 className="font-semibold text-2xl mb-3">Contact</h2>
+              {phone}
+              {email}
+              {website}
+            </div>
+          ) : null
+        }
         {
           hours.length > 0 ? <div>
             <h2 className="font-semibold text-2xl mb-3">Hours</h2>
